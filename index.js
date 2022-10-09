199d8c4473f64557b9ab0646d6697249d55b0cc4
NiushaH <35556570+NiushaH@users.noreply.github.com>
2022-10-08 22:30:00 -0600

    nested obj notes added

--- index.js
+++ index.js
@@ -51,3 +51,27 @@ destructivelyDeleteFromDriverByKey() - this function should work the same as
    your solution.
 
 */
+
+
+// TRAVERSING NESTED OBJECTS  
+// function find(array, criteriaFn) {
+//     let current = array
+//     let next = []
+//     while (current || current === 0) {
+//       if (criteriaFn(current)) {
+//         return current
+//       }
+//       if (Array.isArray(current)) {
+//         for (let i = 0; i < current.length; i++) {
+//           next.push(current[i])
+//         }
+//       }
+//       current = next.shift()
+//     }
+//     return null
+//   }
+
+// const numberCollections = [1, [2, [4, [5, [6]], 3]]]
+
+// find(numberCollections, number => number > 5)
+// find(numberCollections, number => (typeof number === 'number' && number > 5))
